d9037ae7961bfb54a698e0320ff7121295e8ef2b
Aarjeet-1 <arjeetjoshi175@gmail.com>
2025-07-02 17:07:22 +0545

    nvim-cmp aligned with lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,5 +1,4 @@
 return {
-
    {
     "mason-org/mason.nvim",
     config = function()
@@ -12,6 +11,7 @@ return {
  {
    "mason-org/mason-lspconfig.nvim",
    config = function()
+
      require("mason-lspconfig").setup({
 ensure_installed = { "lua_ls", "gopls", "tsserver", "pyright" }
      })
@@ -23,9 +23,10 @@ ensure_installed = { "lua_ls", "gopls", "tsserver", "pyright" }
     "neovim/nvim-lspconfig",
    dependencies = {},
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
-
+capabilities=capabilities,
 
 	settings = {
 	  Lua = {
@@ -35,10 +36,13 @@ ensure_installed = { "lua_ls", "gopls", "tsserver", "pyright" }
 	  },
 	},
       })
-      lspconfig.gopls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.ts_ls.setup({})
-
+      lspconfig.gopls.setup({capabilities=capabilities})
+      lspconfig.pyright.setup({capabilities=capabilities})
+      lspconfig.ts_ls.setup({capabilities=capabilities})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
     end,
   }
